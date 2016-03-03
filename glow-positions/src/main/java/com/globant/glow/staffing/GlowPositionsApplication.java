d703181675da136fc6bf76993645887e5c6e1db0
Akash Bhingole <abhingole@clarice.com>
2016-03-03 18:27:55 +0530

    Added spring boot code for a sample glow-positions module

---
+++ glow-positions/src/main/java/com/globant/glow/staffing/GlowPositionsApplication.java
@@ -0,0 +1,12 @@
+package com.globant.glow.staffing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowPositionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlowPositionsApplication.class, args);
+	}
+}
